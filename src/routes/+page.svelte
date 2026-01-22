3c6606d939f062f6dc286580bf37002fb36c42b0
Daffa Ilhami <mdaffailhami@gmail.com>
2026-01-22 21:45:05 +0800

    feat(styles): define app theme

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<div class="h-100 w-100 border-2 bg-white shadow-lg outline-2"></div>
